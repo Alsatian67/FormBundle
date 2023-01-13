69ce7e18355491f5dd1781d6e943b9d1cb71a851
wucherpfennig <studhalter.daniel@gmail.com>
2023-01-13 14:49:21 +0100

    Fix Symfony 6 Warning - ExtensibleSubscriber (#22)
    * Update ExtensibleSubscriber.php
    
    Added needed return type in order to avoid warning
    
    * Update ExtensibleSubscriber.php

--- Form/Extensions/ExtensibleSubscriber.php
+++ Form/Extensions/ExtensibleSubscriber.php
@@ -23,7 +23,7 @@ class ExtensibleSubscriber implements EventSubscriberInterface
         $this->enabledTypes = $enabledTypes;
     }
     
-    public static function getSubscribedEvents()
+    public static function getSubscribedEvents(): array
     {
         return array(
             FormEvents::PRE_SET_DATA => array('populateAjaxChoices',-50),
