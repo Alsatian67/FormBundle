ba40bbe5dc465e4301e8fe4bde49c821ce9ebc9f
Alsatian67 <romain6772@gmail.com>
2023-01-13 17:41:27 +0100

    Create services.php

---
+++ Resources/config/services.php
@@ -0,0 +1,84 @@
+<?php
+namespace Symfony\Component\DependencyInjection\Loader\Configurator;
+
+use Alsatian\FormBundle\Form\AbstractRoutableType;
+
+use Alsatian\FormBundle\Form\Extensions\ExtensibleExtension;
+use Alsatian\FormBundle\Form\Extensions\ExtensibleSubscriber;
+use Alsatian\FormBundle\Form\AbstractExtensibleChoicesType;
+use Alsatian\FormBundle\Form\ExtensibleChoiceType;
+use Alsatian\FormBundle\Form\ExtensibleDocumentType;
+use Alsatian\FormBundle\Form\ExtensibleEntityType;
+
+use Alsatian\FormBundle\Form\AutocompleteType;
+use Alsatian\FormBundle\Form\DatePickerType;
+use Alsatian\FormBundle\Form\DateTimePickerType;
+
+return function(ContainerConfigurator $container) {
+    $container->services()
+        ->set('alsatian_form.form_type.abstract_routable', AbstractRoutableType::class)
+            ->abstract(true)
+            ->private()
+            ->args([
+                service('router')
+            ])
+            
+        // Extensible types :
+        ->set('alsatian_form.form_extension.extensible', ExtensibleExtension::class)
+            ->private()
+            ->args([
+                service('alsatian_form.form_event_subscriber.extensible')
+            ])
+                     
+        ->set('alsatian_form.form_event_subscriber.extensible', ExtensibleSubscriber::class)
+            ->private()
+            ->args([
+                param('alsatian_form.parameters.extensible.enabled_Types')
+            ])
+
+        ->set('alsatian_form.form_type.abstract_extensible', AbstractExtensibleChoicesType::class)
+            ->parent('alsatian_form.form_type.abstract_routable')
+            ->abstract(true)
+            ->private()
+
+        ->set('alsatian_form.form_type.extensible_choice', ExtensibleChoiceType::class)
+            ->parent('alsatian_form.form_type.abstract_extensible')
+            ->private()
+            ->args([
+                param('alsatian_form.parameters.extensible_choice.attr_class')
+            ]
+
+        ->set('alsatian_form.form_type.extensible_document', ExtensibleDocumentType::class)
+            ->parent('alsatian_form.form_type.abstract_extensible')
+            ->private()
+            ->args([
+                param('alsatian_form.parameters.extensible_document.attr_class')
+            ]
+
+        ->set('alsatian_form.form_type.extensible_entity', ExtensibleEntityType::class)
+            ->parent('alsatian_form.form_type.abstract_extensible')
+            ->private()
+            ->args([
+                param('alsatian_form.parameters.extensible_entity.attr_class')
+            ]
+                    
+        // Other types 
+        ->set('alsatian_form.form_type.autocomplete', AutocompleteType::class)
+            ->parent('alsatian_form.form_type.abstract_routable')
+            ->args([
+                param('alsatian_form.parameters.autocomplete.attr_class')
+            ]
+
+        ->set('alsatian_form.form_type.date_picker', DatePickerType::class)
+            ->args([
+                service('request_stack'),
+                param('alsatian_form.parameters.date_picker.attr_class')
+            ]
+
+        ->set('alsatian_form.form_type.datetime_picker', DateTimePickerType::class)
+            ->args([
+                service('request_stack'),
+                param('alsatian_form.parameters.datetime_picker.attr_class')
+            ]
+    ;
+};
