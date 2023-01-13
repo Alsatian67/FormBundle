032fee1c004babdb25ee1cd7662a40ce2835d200
Alsatian67 <romain6772@gmail.com>
2023-01-13 15:09:28 +0100

    Update DatePickerType.php

--- Form/DatePickerType.php
+++ Form/DatePickerType.php
@@ -3,10 +3,9 @@ namespace Alsatian\FormBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 
-use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\OptionsResolver\Options;
-
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\OptionsResolver\Options;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class DatePickerType extends AbstractType
 {
